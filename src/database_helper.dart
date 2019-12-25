c864cfe5d1d307c98925b2b818472ff870d052e6
One-Nub <38899321+One-Nub@users.noreply.github.com>
2019-12-25 13:27:04 -0600

    Adding database methods & config

---
+++ src/database_helper.dart
@@ -0,0 +1,71 @@
+import 'package:mysql1/mysql1.dart';
+
+class database_helper {
+  var connection;
+
+  Future<void> connect(String username, String password, String host,
+      String database, int port) async {
+    var user_config = new ConnectionSettings(
+        user: username,
+        password: password,
+        host: host,
+        db: database,
+        port: port);
+    connection = await MySqlConnection.connect(user_config);
+  }
+
+  Future<void> add_cookies(int userID, int numCookies, int guildID) async {
+    //No user input is ever used here so sanitization isn't as concerning, though
+    //typically good in practice
+    var queryExistance = await connection.query(
+        "SELECT EXISTS (SELECT * FROM `$guildID` WHERE user_id = $userID)");
+    int result = queryExistance.first.values[0];
+    if (result == 0) {
+      //Row does not exist
+      await connection.query("INSERT INTO `$guildID` SET "
+          "user_id = $userID, "
+          "total_cookies = $numCookies, "
+          "available_cookies = $numCookies");
+    } else {
+      //Row exists
+      await connection.query("UPDATE `$guildID` SET "
+          "total_cookies = total_cookies + $numCookies, "
+          "available_cookies = available_cookies + $numCookies "
+          "WHERE user_id = $userID");
+    }
+  }
+
+  Future<void> remove_cookies(int userID, int numCookies, int guildID) async {
+    var queryExistance = await connection.query(
+        "SELECT EXISTS (SELECT * FROM `$guildID` WHERE user_id = $userID)");
+    int result = queryExistance.first.values[0];
+    if (result != 0) {
+      await connection.query("UPDATE `$guildID` SET "
+        "available_cookies = available_cookies - $numCookies "
+        "WHERE user_id = $userID");
+    }
+  }
+
+  Future<int> get_cookies(int userID, int guildID) async {
+    var queryExistance = await connection.query(
+        "SELECT EXISTS (SELECT * FROM `$guildID` WHERE user_id = $userID)");
+    int result = queryExistance.first.values[0];
+    if (result != 0) {
+      var row = await connection.query(
+          "SELECT available_cookies FROM `$guildID` WHERE user_id = $userID");
+      var numCookies = row.first.values[0];
+      return numCookies;
+    }
+    return 0;
+  }
+
+  Future<void> increase_level(int userID, int numCookies, int guildID) async {}
+
+  Future<void> create_table(int guildID) async {
+    connection.query("CREATE TABLE IF NOT EXISTS `$guildID` ("
+        "user_id BIGINT UNSIGNED PRIMARY KEY, "
+        "total_cookies MEDIUMINT UNSIGNED, "
+        "available_cookies MEDIUMINT UNSIGNED, "
+        "level SMALLINT)");
+  }
+}
--- src/main.dart
+++ src/main.dart
@@ -6,9 +6,11 @@ import 'package:nyxx/Vm.dart';
 import 'package:nyxx.commands/commands.dart' as cmd; //rewrite locally cloned
 
 import 'cmds/commands_list.dart'; //ignore: unused_import
+import 'database_helper.dart';
 
 Nyxx bot;
 var prefixHandler;
+var db;
 
 Future<void> main() async {
   var timer = Stopwatch();
@@ -18,28 +20,43 @@ Future<void> main() async {
 
   String token = botConfig.token;
   String prefix = botConfig.prefix;
-  String mention= "";
+  String mention = "";
+  List<Snowflake> admins = [Snowflake(156872400145874944)];
+
+  db = database_helper();
+  await db.connect(
+      botConfig.database_config.username, 
+      botConfig.database_config.password, 
+      botConfig.database_config.host, 
+      botConfig.database_config.databaseName, 
+      botConfig.database_config.port);
 
   bot = NyxxVm(token);
-  List<Snowflake> admins = [Snowflake(156872400145874944)];
-  
   prefixHandler = cmd.CommandsFramework(bot, prefix: prefix, admins: admins)
     ..discoverCommands();
-  var mentionHandler = cmd.CommandsFramework(bot, prefix: mention, admins: admins)
-    ..discoverCommands();
-  
+  var mentionHandler =
+      cmd.CommandsFramework(bot, prefix: mention, admins: admins)
+        ..discoverCommands();
+
   bot.onReady.listen((e) {
     timer.stop();
     print("Ready in ${bot.guilds.count} guild(s) as "
-      "${bot.self.username + "#" + bot.self.discriminator}");
+        "${bot.self.username + "#" + bot.self.discriminator}");
     print("It took ${timer.elapsed.inSeconds} seconds to start up");
 
-    var presence = Presence.of("to some happy tunes", type: PresenceType.listening);
+    var presence =
+        Presence.of("to some happy tunes", type: PresenceType.listening);
     bot.self.setPresence(game: presence);
 
     mention = bot.self.mention;
     mentionHandler.prefix = mention;
     cookieTriggerListener();
+  });
+
+  bot.onGuildCreate.listen((e) async {
+    var guildID = e.guild.id.toInt();
+    await db.create_table(guildID);
+    print("Joined guild - ${e.guild.name} with ID of $guildID");
   });
 }
 
