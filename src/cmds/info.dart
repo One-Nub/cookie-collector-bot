68182c59ed846f9edbfc3ef320e1cf5cca6c8ee1
One-Nub <38899321+One-Nub@users.noreply.github.com>
2020-01-10 21:22:15 -0600

    Changing how uptime is displayed

--- src/cmds/info.dart
+++ src/cmds/info.dart
@@ -5,9 +5,16 @@ part of commands;
 Future<void> info(CommandContext ctx) async {
   var botName = "${bot.self.username + "#" + bot.self.discriminator}";
   var memUsage = (ProcessInfo.currentRss / 1024 / 1024).toStringAsFixed(2);
+
+  //Don't you love string manip?
+  String temp = bot.uptime.toString();
+  var splittedTemp = temp.split(":");
+  var uptime = "${splittedTemp[0]}h "
+      "${splittedTemp[1]}m "
+      "${splittedTemp[2].substring(0, 2)}s";
+
   var embed = await EmbedBuilder()
-    ..addField(
-        name: "Uptime", content: "${bot.uptime.inMinutes} min", inline: true)
+    ..addField(name: "Uptime", content: uptime, inline: true)
     //Big thanks to lib dev (l7ssha) and the power of ctrl + f for memory usage
     ..addField(
         name: "DartVM memory usage", content: "$memUsage MB", inline: true)
