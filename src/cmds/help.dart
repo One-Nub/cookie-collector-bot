fa3f71ec7c19e991bebfb671dadf3d48e4654485
One-Nub <38899321+One-Nub@users.noreply.github.com>
2020-02-07 23:39:13 -0600

    Some efficiency changes, not like they're needed and formatting
    totally wasn't trying to test a solution to an issue the library caused 
    by changing my code... also made some formatting changes so there's that 
    too

--- src/cmds/help.dart
+++ src/cmds/help.dart
@@ -3,36 +3,34 @@ part of commands;
 @Command("help")
 Future<void> help(CommandContext ctx) async {
   String commands =
-      "`.daily` ➙ collect a set amount of cookies once a day\n"
-      "`.eat` ➙ eat a cookie! that's literally it\n"
-      "`.give <user> <amt>` ➙ give the user of your choosing some cookies\n"
-      "`.help` ➙ view this help prompt!\n"
-      "`.info` ➙ get some information about the bot\n"
-      "`.leaderboard` ➙ view the top 15 people with the most cookies\n"
-      "`.ping` ➙ view the latency between discord and the bot\n"
-      "`.rob <user>` ➙ take some cookies from someone\n"
-      "`.stats [user]` ➙ view your stored cookie count and level, or someone elses";
+    "`.daily` ➙ collect a set amount of cookies once a day\n"
+    "`.eat` ➙ eat a cookie! that's literally it\n"
+    "`.give <user> <amt>` ➙ give the user of your choosing some cookies\n"
+    "`.help` ➙ view this help prompt!\n"
+    "`.info` ➙ get some information about the bot\n"
+    "`.leaderboard` ➙ view the top 15 people with the most cookies\n"
+    "`.ping` ➙ view the latency between discord and the bot\n"
+    "`.rob <user>` ➙ take some cookies from someone\n"
+    "`.stats [user]` ➙ view your stored cookie count and level, or someone elses";
 
-  String adminCommands =
-      "`.say <channel> <message>` ➙ have the bot say something in a channel\n";
-  DMChannel authorDM = await ctx.author.dmChannel;
-
-  if (ctx.author.id.toInt() == 156872400145874944) {
-    adminCommands +=
-        "`.generate <user> <amt>` ➙ give the user infinite cookies\n";
+  StringBuffer buffer = new StringBuffer();
+  buffer.write("`.say <channel> <message>` ➙ have the bot say something in a channel\n");
+  if (await ctx.author.id.toInt() == 156872400145874944) {
+    buffer.write("`.generate <user> <amt>` ➙ give the user infinite cookies\n");
   }
 
   var helpEmbed = await EmbedBuilder()
     ..addField(name: "Commands", content: commands);
   if (admins.contains(ctx.author.id)) {
-    await helpEmbed.addField(name: "Admin Commands", content: adminCommands);
+    await helpEmbed.addField(name: "Admin Commands", content: buffer.toString());
   }
 
   helpEmbed.title = "Help!";
   helpEmbed.description = "Welcome to the help prompt! Hopefully you find what "
-      "you are looking for! \n\n If you happen to find a bug of some sort, "
-      "please DM Nub#8399 and he will look into it!";
+    "you are looking for! \n\n If you happen to find a bug of some sort, "
+    "please DM Nub#8399 and he will look into it!";
 
+  var authorDM = await ctx.author.dmChannel;
   if(ctx.channel is DMChannel) {
     ctx.channel.send(embed: helpEmbed);
   }
