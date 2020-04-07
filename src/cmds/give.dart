de9077e03b0045845deeefdbf1bec73a7c214b87
One-Nub <38899321+One-Nub@users.noreply.github.com>
2020-04-06 22:59:11 -0500

    Adding confirmation for cookie counts over 50 & prevent giving to self

--- src/cmds/give.dart
+++ src/cmds/give.dart
@@ -7,16 +7,41 @@ Future<void> give(CommandContext ctx, [User reciever, int cookieCount]) async {
   if(reciever is int) {
     reciever = ctx.guild.members[Snowflake(reciever)];
   }
-  
+
+  if(reciever.id == ctx.author.id) {
+    await ctx.message.reply(content: "You can't give yourself cookies, that's called"
+    " either buying or baking them.");
+    return;
+  }
+
   int authorCookieCount = await db.get_cookies(ctx.author.id.toInt(), ctx.guild.id.toInt());
-  
   if (cookieCount == null) {
     ctx.reply(content: "${ctx.author.mention} I need an amount of cookies to give!");
     return;
   }
-  else if(cookieCount <= 0 || cookieCount > 50 || cookieCount > authorCookieCount) {
+  else if(cookieCount <= 0 || cookieCount > authorCookieCount) {
     ctx.reply(content: "${ctx.author.mention} You can't give that many cookies!");
     return;
+  }
+
+  if(cookieCount >= 50)
+  {
+    var botConfirm = await ctx.reply(content: "Please confirm that you want to send"
+    " **${reciever.tag}** `$cookieCount cookies`. (`Yes/No`)");
+
+    var userConfirmStream = await ctx.nextMessagesWhere(
+      (msg) => msg.message.author.id == (ctx.author.id), limit: 1);
+
+    MessageEvent userConfirm = await userConfirmStream.first;
+    if(!userConfirm.message.content.toLowerCase().startsWith("y")) {
+      ctx.replyTemp(new Duration(seconds: 5), content: "Cancelled.");
+      botConfirm.delete();
+      ctx.message.delete();
+      return;
+    }
+
+    botConfirm.delete();
+    userConfirm.message.delete();
   }
 
   await db.add_cookies(reciever.id.toInt(), cookieCount, ctx.guild.id.toInt());
