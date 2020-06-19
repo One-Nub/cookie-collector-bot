3745fa2ff3abbda31860b22aa54221c17260f14c
One-Nub <38899321+One-Nub@users.noreply.github.com>
2020-06-19 11:20:27 -0500

    Updating ChannelArgument to support recent changes in nyxx

--- src/framework/argument/ChannelArgument.dart
+++ src/framework/argument/ChannelArgument.dart
@@ -2,7 +2,7 @@ part of framework;
 
 /// Facilitates the ability of getting a Channel from a passed input.
 ///
-/// Accepted types: CachelessTextChannel, CacheVoiceChannel, CategoryChannel
+/// Accepted types: CachelessTextChannel, CacheTextChannel, CacheVoiceChannel, CategoryChannel
 /// Throws [GuildContextRequiredException] when run from a non-guild context.
 /// Throws [MissingArgumentException] if an argument cannot be found.
 /// Throws [InvalidChannelException] if a valid channel cannot be found.
@@ -42,7 +42,13 @@ class ChannelArgument<T extends Channel> extends Argument {
 
       //O(n) for every channel of T
       for (var channel in guildChannels) {
-        if (channel.type == ChannelType.text && T == CachelessTextChannel) {
+        if (channel.type == ChannelType.text && T == CacheTextChannel) {
+          channel = channel as CacheTextChannel;
+          if (channel.name.toLowerCase() == message.toLowerCase()) {
+            channelID = channel.id.id;
+            break;
+          }
+        } else if(channel.type == ChannelType.text && T == CachelessTextChannel) {
           channel = channel as CachelessTextChannel;
           if (channel.name.toLowerCase() == message.toLowerCase()) {
             channelID = channel.id.id;
