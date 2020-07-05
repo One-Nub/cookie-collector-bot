aa50c21910e13cba6fa0ef8633215a41d3cda760
One-Nub <38899321+One-Nub@users.noreply.github.com>
2020-07-05 01:02:23 -0500

    Convert to guild specific cooldown

--- src/framework/commands/Cooldown.dart
+++ src/framework/commands/Cooldown.dart
@@ -2,28 +2,31 @@ part of framework;
 
 mixin Cooldown {
   static Duration cooldownDuration = Duration(seconds: 5);
-  static HashMap<Snowflake, DateTime> _cooldownCache = new HashMap();
+  static HashMap<Snowflake, HashMap<Snowflake, DateTime>> _cooldownCache = new HashMap();
   
-  static void applyCooldown(Snowflake userSnowflake) {
+  static void applyCooldown(Snowflake guildID, Snowflake userSnowflake) {
     DateTime offsetDuration = DateTime.now().add(cooldownDuration);
-    _cooldownCache[userSnowflake] = offsetDuration;
+    _cooldownCache[guildID] ??= new HashMap();
+
+    _cooldownCache[guildID]![userSnowflake] = offsetDuration;
   }
 
   ///True if the user is currently on cooldown, false otherwise.
-  static bool isCooldownActive(Snowflake userSnowflake) {
-    if(!_cooldownCache.containsKey(userSnowflake)) return false;
+  static bool isCooldownActive(Snowflake guildID, Snowflake userSnowflake) {
+    if(!_cooldownCache.containsKey(guildID)) return false;
+    if(!_cooldownCache[guildID]!.containsKey(userSnowflake)) return false;
 
-    return _cooldownCache[userSnowflake]!.isAfter(DateTime.now());
+    return _cooldownCache[guildID]![userSnowflake]!.isAfter(DateTime.now());
   }
 
-  static DateTime? getCooldownTime(Snowflake userSnowflake) {
-    return _cooldownCache[userSnowflake];
+  static DateTime? getCooldownTime(Snowflake guildID, Snowflake userSnowflake) {
+    return _cooldownCache[guildID]?[userSnowflake];
   }
 
-  static String getRemainingTime(Snowflake userSnowflake) {
-    if(_cooldownCache.containsKey(userSnowflake)) {
-      return DateTimeFormat.relative(_cooldownCache[userSnowflake], 
-        minUnitOfTime: UnitOfTime.millisecond);
+  static String getRemainingTime(Snowflake guildID, Snowflake userSnowflake) {
+    if(_cooldownCache.containsKey(guildID) && _cooldownCache[guildID]!.containsKey(userSnowflake)) {
+      return DateTimeFormat.relative(_cooldownCache[guildID]![userSnowflake], 
+        round: false, levelOfPrecision: 5, excludeWeeks: true);
     }
     return "Cooldown is not applied.";
   }
