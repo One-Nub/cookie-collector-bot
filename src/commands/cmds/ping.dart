65a3ba480ab2bb46c1c29a9c85f553a28b52c689
One-Nub <38899321+One-Nub@users.noreply.github.com>
2020-12-22 17:36:51 -0600

    Resolve possibility of no bot member in guild

--- src/commands/cmds/ping.dart
+++ src/commands/cmds/ping.dart
@@ -4,7 +4,10 @@ class Ping {
   static Future<bool> preRunChecks(CommandContext ctx) async {
     if(ctx.guild == null) return false;
 
-    Member botMember = ctx.guild!.selfMember!;
+    Member? botMember = ctx.guild!.selfMember;
+    if(botMember == null) {
+      botMember = await ctx.guild!.fetchMember(ctx.client.self.id);
+    }
     GuildChannel channel = ctx.channel as GuildChannel;
     var botPerms = await channel.effectivePermissions(botMember);
     
@@ -18,7 +21,7 @@ class Ping {
     Stopwatch timer = Stopwatch();
     timer.start();
     var msg = await ctx.channel.sendMessage(content: "Pinging...");
-    timer.stop;
+    timer.stop();
     msg.edit(content: ":stopwatch: Pong! `${timer.elapsedMilliseconds}ms`");
   }
 }
