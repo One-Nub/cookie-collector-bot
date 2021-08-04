fba1273af8846b8d0346a920e550ceff56795db3
One-Nub <38899321+One-Nub@users.noreply.github.com>
2021-08-04 16:39:46 -0500

    Change commands package to modules package

---
+++ lib/modules.dart
@@ -0,0 +1,22 @@
+//Admin Commands
+export 'src/modules/admin_commands/generate.dart';
+export 'src/modules/admin_commands/say.dart';
+
+//Chat Collection
+export 'src/modules/chat_collection/ChannelListener.dart';
+export 'src/modules/chat_collection/CollectionMessage.dart';
+export 'src/modules/chat_collection/GuildListener.dart';
+
+//General Commands
+export 'src/modules/general_commands/daily.dart';
+export 'src/modules/general_commands/eat.dart';
+export 'src/modules/general_commands/give.dart';
+// export 'src/modules/general_commands/help.dart';
+export 'src/modules/general_commands/info.dart';
+export 'src/modules/general_commands/leaderboard.dart';
+export 'src/modules/general_commands/ping.dart';
+export 'src/modules/general_commands/rob.dart';
+export 'src/modules/general_commands/stats.dart';
+
+
+
