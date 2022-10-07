74ec22b35f1e1bfbe07481ad203e9f78e0d360c1
Nub <38899321+One-Nub@users.noreply.github.com>
2022-10-07 03:40:08 -0500

    Add OnyxChat implementation and imports

--- lib/src/core/CCBot.dart
+++ lib/src/core/CCBot.dart
@@ -1,11 +1,26 @@
 import 'package:nyxx/nyxx.dart';
 import 'package:logging/logging.dart';
+import 'package:onyx_chat/onyx_chat.dart';
 
-import '../new_modules/chat_collection/on_message.dart';
+import '../new_modules/chat_collection/on_message.dart' as cc;
+
+import '../new_modules/admin_commands/generate.dart';
+import '../new_modules/admin_commands/say.dart';
+
+import '../new_modules/general_commands/daily.dart';
+import '../new_modules/general_commands/eat.dart';
+import '../new_modules/general_commands/give.dart';
+import '../new_modules/general_commands/help.dart';
+import '../new_modules/general_commands/info.dart';
+import '../new_modules/general_commands/leaderboard.dart';
+import '../new_modules/general_commands/ping.dart';
+import '../new_modules/general_commands/rob.dart';
+import '../new_modules/general_commands/stats.dart';
 
 class CCBot {
   final String token;
   late final INyxxWebsocket gateway;
+  late final OnyxChat onyxChat;
 
   CCBot({required this.token});
 
@@ -19,18 +34,23 @@ class CCBot {
         cacheOptions: cacheOptions)
       ..registerPlugin(Logging())
       ..registerPlugin(CliIntegration())
-      ..registerPlugin(IgnoreExceptions());
+      ..registerPlugin(IgnoreExceptions())
+      ..connect();
 
     gateway.eventsWs.onReady.listen((event) {
       gateway.setPresence(PresenceBuilder.of(
           status: UserStatus.online, activity: ActivityBuilder("the chat go by...", ActivityType.watching)));
     });
 
-    gateway.eventsWs.onMessageReceived.listen((event) {
-      onMessageEvent(event);
-    });
+    onyxChat = OnyxChat(gateway, prefix: ".");
+    onyxChat.addCommandList([GenerateCommand()]);
 
-    await gateway.connect();
+    gateway.eventsWs.onMessageReceived.listen((event) {
+      if (event.message.author.bot) return;
+
+      onyxChat.dispatchIMessage(event.message);
+      cc.onMessageEvent(event);
+    });
   }
 }
 
