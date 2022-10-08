32a84305646142e64d371c5d6c28d1f2d0c224fd
Nub <38899321+One-Nub@users.noreply.github.com>
2022-10-08 17:08:28 -0500

    Remove residual commented code

--- lib/src/core/CCBot.dart
+++ lib/src/core/CCBot.dart
@@ -82,33 +82,3 @@ class CCBot {
     interactions = IInteractions.create(WebsocketInteractionBackend(gateway));
   }
 }
-
-// class CCBot extends Nyxx {
-//   final CCDatabase database;
-//   late List<Snowflake> admins = [];
-//   HashMap<Snowflake, GuildListener> guildListeners = HashMap();
-
-//   CCBot(String token, int intents, this.database,
-//       {ClientOptions? options,
-//       CacheOptions? cacheOptions,
-//       bool ignoreExceptions = true,
-//       bool useDefaultLogger = true,
-//       admins})
-//       : super(token, intents,
-//             options: options,
-//             cacheOptions: cacheOptions,
-//             ignoreExceptions: ignoreExceptions,
-//             useDefaultLogger: useDefaultLogger);
-
-//   bool checkForGuildListener(Snowflake guildID) {
-//     return guildListeners.containsKey(guildID);
-//   }
-
-//   void addGuildListener(Snowflake guildID, GuildListener gl) {
-//     guildListeners.putIfAbsent(guildID, () => gl);
-//   }
-
-//   void removeGuildListener(Snowflake guildID, GuildListener gl) {
-//     guildListeners.removeWhere((key, value) => key == guildID);
-//   }
-// }
